bd8141d47fa52591b02aa07008583fb36c512144
mahdihp.devsc <messenger123456>
2024-03-24 18:31:48 +0330

    add entites

--- ent/schema/role.go
+++ ent/schema/role.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+)
 
 // Role holds the schema definition for the Role entity.
 type Role struct {
@@ -9,10 +14,22 @@ type Role struct {
 
 // Fields of the Role.
 func (Role) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Int16("id"),
+		field.String("name").MaxLen(25).Unique(),
+		field.String("description").MaxLen(100).Optional().Nillable(),
+	}
+}
+
+func (Role) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("id").Unique(),
+	}
 }
 
 // Edges of the Role.
 func (Role) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.To("users", User.Type),
+	}
 }
--- ent/schema/user.go
+++ ent/schema/user.go
@@ -2,7 +2,11 @@ package schema
 
 import (
 	"entgo.io/ent"
+	"entgo.io/ent/dialect"
+	"entgo.io/ent/schema/edge"
 	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/index"
+	"time"
 )
 
 // User holds the schema definition for the User entity.
@@ -14,10 +18,32 @@ type User struct {
 func (User) Fields() []ent.Field {
 	return []ent.Field{
 		field.Int64("id"),
+		field.String("mobile").Unique().MaxLen(11),
+		field.String("username").MaxLen(32).Optional().Nillable(),
+		field.Bool("active").Default(true).Comment("فعال بودن"),
+		field.Bool("deleted").Default(false).Comment("حذف منطقی"),
+
+		field.Time("created_at").
+			SchemaType(map[string]string{
+				dialect.Postgres: "timestamp(6)", // Override Postgres.
+			}).Default(time.Now).Immutable(),
+
+		field.Time("updated_at").
+			SchemaType(map[string]string{
+				dialect.Postgres: "timestamp(6)", // Override Postgres.
+			}).Default(time.Now()),
+	}
+}
+
+func (User) Indexes() []ent.Index {
+	return []ent.Index{
+		index.Fields("id").Unique(),
 	}
 }
 
 // Edges of the User.
 func (User) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("roles", Role.Type).Ref("users"),
+	}
 }
