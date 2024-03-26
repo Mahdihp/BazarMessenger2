af9db6b49279a48011ea2636092706620fdd8b9d
mahdihp.devsc <messenger123456>
2024-03-26 18:43:20 +0330

    add entites

--- services/user/user_service.go
+++ services/user/user_service.go
@@ -1,7 +1,21 @@
 package user
 
-import "BazarMessenger/repository/postgres/user"
+import (
+	"BazarMessenger/dto"
+	"BazarMessenger/repository/postgres/user"
+)
 
-type User struct {
-	Repo user.UserRepository
+type UserServiceRepositort interface {
+	Register() (dto.UserInfo, error)
+}
+type UserService struct {
+	repo user.UserRepository
+}
+
+func New(repo user.UserRepository) UserService {
+	return UserService{repo: repo}
+}
+
+func (this *UserService) Register() (dto.UserInfo, error) {
+	return this.repo.Register()
 }
